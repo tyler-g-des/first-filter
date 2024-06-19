44086d9094a24afab9786a0bc91e69adb00752d9
Tyler Diaz <tyler_diaz@bhd.com.do>
2024-06-19 12:03:55 -0400

    cambio archivo main maquina size no soprtada en susbcripcion gratuita

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   default_node_pool {
     name       = "default"
     node_count = 2
-    vm_size    = "Standard_DS2_v2"
+    vm_size    = "Standard_DS2_v3"
   }
 
   identity {
