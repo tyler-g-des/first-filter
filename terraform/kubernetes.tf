fdeb1e937f15ce59fc0f746a94ae072548da2a8b
Tyler Diaz <tyler_diaz@bhd.com.do>
2024-06-27 22:01:20 -0400

    comentar tareas para probar IAC

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -1,3 +1,17 @@
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.cluster_ca_certificate)
+}
+
+resource "kubernetes_namespace" "java_app_namespace" {
+  metadata {
+    name = "java-app-namespace"
+  }
+  depends_on = [azurerm_kubernetes_cluster.aks_cluster]
+}
+
 resource "kubernetes_deployment" "backend_spring" {
   metadata {
     name      = "backend-spring"
@@ -12,8 +26,8 @@ resource "kubernetes_deployment" "backend_spring" {
       match_labels = {
         app = "backend-spring"
       }
-    }    
-  
+    }
+
     template {
       metadata {
         labels = {
@@ -24,8 +38,7 @@ resource "kubernetes_deployment" "backend_spring" {
       spec {
         container {
           name  = "backend-spring"
-          image = "tyler0128/spring:latest"
-
+          image = "nginx:1.14.2" # Reemplaza con la imagen de tu aplicación
           port {
             container_port = 80
           }
@@ -35,22 +48,39 @@ resource "kubernetes_deployment" "backend_spring" {
   }
 }
 
-resource "kubernetes_service" "aplication_app_service" {
+resource "kubernetes_service" "application_app_service" {
   metadata {
-    name      = "backend-spring-service"
+    name      = "application-service"
     namespace = kubernetes_namespace.java_app_namespace.metadata[0].name
   }
-  depends_on = [kubernetes_namespace.java_app_namespace]
+
   spec {
     selector = {
       app = "backend-spring"
     }
 
     port {
+      protocol    = "TCP"
       port        = 80
       target_port = 80
     }
+  }
+}
+resource "kubernetes_service" "application_app_service" {
+  metadata {
+    name      = "application-service"
+    namespace = kubernetes_namespace.java_app_namespace.metadata[0].name
+  }
 
-    type = "LoadBalancer"
+  spec {
+    selector = {
+      app = "backend-spring"
+    }
+
+    port {
+      protocol    = "TCP"
+      port        = 80
+      target_port = 80
+    }
   }
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -2,13 +2,6 @@ provider "azurerm" {
   features {}
 }
 
-provider "kubernetes" {
-  host                   = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.host
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.cluster_ca_certificate)
-}
-
 resource "azurerm_resource_group" "aks_rg" {
   name     = "aks-resource-group"
   location = "East US 2"
