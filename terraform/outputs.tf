6cfe1cf3179a7cdf75e768a3c37d16c58e2f4e78
Tyler Diaz <tyler_diaz@bhd.com.do>
2024-06-29 14:13:07 -0400

    cambios kubernetes

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "kube_config" {
-  value     = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
+  value     = azurerm_kubernetes_cluster.aks-cluster.kube_config_raw
   sensitive = true
 }
