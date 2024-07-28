d6aeed364925c6d30d180be08c6724ac38797b69
Tyler Diaz <tyler_diaz@bhd.com.do>
2024-07-28 16:14:03 -0400

    se cambio la version

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
  terraform {
-  required_version = ">= 0.11" 
+  required_version = ">= 2.10.0"
  backend "azurerm" {
   storage_account_name = "__terraformstorageaccount__"
     container_name       = "terraform"
