5f073f3093d749edff05d9b437226a10d748fe0b
Tyler Diaz <tyler_diaz@bhd.com.do>
2024-07-27 23:25:50 -0400

    se agrego app service plan y app service

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,6 +18,25 @@ resource "azurerm_resource_group" "aks-resource-group" {
   location = "East US 2"
 }
 
+resource "azurerm_app_service_plan" "aks-resource-group" {
+  name                = "__appserviceplan__"
+  location            = "${azurerm_resource_group.aks-resource-group.location}"
+  resource_group_name = "${azurerm_resource_group.aks-resource-group.name}"
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+resource "azurerm_app_service" "aks-resource-group" {
+  name                = "__appservicename__"
+  location            = "${azurerm_resource_group.aks-resource-group.location}"
+  resource_group_name = "${azurerm_resource_group.aks-resource-group.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.aks-resource-group.id}"
+
+}
+
 resource "azurerm_kubernetes_cluster" "aks-cluster" {
   name                = "aks-cluster"
   location            = azurerm_resource_group.aks-resource-group.location
