79e9ba20a6e0f25b3012a6663acb67db549ed590
Tyler Diaz <tyler_diaz@bhd.com.do>
2024-07-28 16:13:16 -0400

    se modifico el ingress

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -146,23 +146,158 @@ resource "kubernetes_service" "application_app_service" {
 }
 
 ## INGRESS 
-resource "kubernetes_ingress" "application_ingress" {
+# Create public IPs
+resource "azurerm_public_ip" "public_ip" {
+  name                =  "tylerdesops-publicip"
+  location            = azurerm_kubernetes_cluster.aks-cluster.location
+  resource_group_name = azurerm_kubernetes_cluster.aks-cluster.node_resource_group
+  allocation_method   = "Static"
+  sku = "Standard"
+  sku_tier = "Regional"
+}
+
+#Create DNS
+resource "azurerm_dns_zone" "dns" {
+  name                = "tylerdesops.com"
+  resource_group_name = azurerm_resource_group.aks-resource-group.name
+}
+
+resource "azurerm_dns_a_record" "rc" {
+  name                = "@"
+  zone_name           = azurerm_dns_zone.dns.name
+  resource_group_name = azurerm_resource_group.aks-resource-group.name
+  ttl                 = 1
+  records             = ["${azurerm_public_ip.public_ip.ip_address}"]
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = azurerm_kubernetes_cluster.aks.kube_config.0.host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.aks-cluster.kube_config.0.client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.aks-cluster.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks-cluster.kube_config.0.cluster_ca_certificate)
+  }
+}
+
+# Install Nginx Ingress using Helm Chart
+resource "helm_release" "ingress-nginx" {
+  name       = "ingress-nginx"
+  namespace = "ingress-basic"
+  create_namespace = true
+  chart      = "./charts/ingress-nginx-4.3.0.tgz" #https://github.com/kubernetes/ingress-nginx/releases
+  timeout                 = 600
+  reuse_values            = true
+  recreate_pods           = true
+  cleanup_on_fail         = true
+  wait                    = true
+  verify                  = false
+  set {
+    name  = "controller.replicaCount"
+    value = 1
+  }
+
+  set {
+    name  = "controller.nodeSelector.kubernetes\\.io/os"
+    value = "linux"
+  }
+
+  set {
+    name  = "defaultBackend.nodeSelector.kubernetes\\.io/os"
+    value = "linux"
+  }
+
+  set {
+    name  = "controller.service.loadBalancerIP"
+    value = "${azurerm_public_ip.public_ip.ip_address}"
+  }
+}
+
+resource "kubernetes_ingress" "hello_world_ingress" {
   metadata {
-    name      = "application-ingress"
-    namespace = kubernetes_namespace.java_app_namespace.metadata[0].name
+    name = "hello-world-ingress"
     annotations = {
-      "nginx.ingress.kubernetes.io/rewrite-target" = "/"
+      "nginx.ingress.kubernetes.io/ssl-redirect"  = "false"
+      "nginx.ingress.kubernetes.io/use-regex"      = "true"
+      "nginx.ingress.kubernetes.io/rewrite-target"  = "/$2"
     }
   }
 
   spec {
+    ingress_class_name = "nginx"
+
     rule {
       http {
         path {
-          path = "/"
+          path = "/hello-world-one(/|$)(.*)"
+          path_type = "Prefix"
+
           backend {
-            service_name = kubernetes_service.application_app_service.metadata[0].name
-            service_port = 8080
+            service {
+              name = "aks-helloworld-one"
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+
+        path {
+          path = "/hello-world-two(/|$)(.*)"
+          path_type = "Prefix"
+
+          backend {
+            service {
+              name = "aks-helloworld-two"
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+
+        path {
+          path = "/(.*)"
+          path_type = "Prefix"
+
+          backend {
+            service {
+              name = "aks-helloworld-one"
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_ingress" "hello_world_ingress_static" {
+  metadata {
+    name        = "hello-world-ingress-static"
+    annotations = {
+      "nginx.ingress.kubernetes.io/ssl-redirect" = "false"
+      "nginx.ingress.kubernetes.io/rewrite-target" = "/static/$2"
+    }
+  }
+
+  spec {
+    ingress_class_name = "nginx"
+
+    rule {
+      http {
+        path {
+          path = "/static(/|$)(.*)"
+          path_type = "ImplementationSpecific"  # Cambiado de "Prefix" a "ImplementationSpecific"
+
+          backend {
+            service {
+              name = "aks-helloworld-one"
+              port {
+                number = 80
+              }
+            }
           }
         }
       }
--- terraform/main.tf
+++ terraform/main.tf
@@ -37,51 +37,6 @@ resource "azurerm_resource_group" "aks-resource-group" {
 
 # }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = "app-service-vnet"
-  location            = azurerm_resource_group.aks-resource-group.location
-  resource_group_name = azurerm_resource_group.aks-resource-group.name
-  address_space       = ["10.0.0.0/16"]
-}
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "app-service-subnet"
-  resource_group_name  = azurerm_resource_group.aks-resource-group.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_app_service_plan" "app_service_plan" {
-  name                = "__appserviceplan__"
-  location            = azurerm_resource_group.aks-resource-group.location
-  resource_group_name = azurerm_resource_group.aks-resource-group.name
-
-  sku {
-    tier = "Free"
-    size = "F1"
-  }
-}
-
-resource "azurerm_app_service" "app_service" {
-  name                = "__appservicename__"
-  location            = azurerm_resource_group.aks-resource-group.location
-  resource_group_name = azurerm_resource_group.aks-resource-group.name
-  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
-
-  site_config {
-    linux_fx_version = "NODE|14-lts"  # Utiliza Node.js para el proxy
-  }
-
-  app_settings = {
-    "WEBSITE_NODE_DEFAULT_VERSION" = "14-lts"
-  }
-}
-
-resource "azurerm_app_service_virtual_network_swift_connection" "app_service_vnet" {
-  app_service_id = azurerm_app_service.app_service.id
-  subnet_id      = azurerm_subnet.subnet.id
-}
-
 resource "azurerm_kubernetes_cluster" "aks-cluster" {
   name                = "aks-cluster"
   location            = azurerm_resource_group.aks-resource-group.location
